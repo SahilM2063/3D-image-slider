f8f07de9c8356df6f346da7d26550d0839877240
rajintera <raj@intera.us>
2024-10-05 21:45:33 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,46 @@
+export const slides = [
+    {
+        title : "Neon",
+        id: "CG30128"
+    },
+    {
+        title : "Volt",
+        id: "CG30129"
+    },
+    {
+        title : "Echo",
+        id: "CG30130"
+    },
+    {
+        title : "Mors",
+        id: "CG30131"
+    },
+    {
+        title : "Earthos",
+        id: "CG30132"
+    },
+    {
+        title : "Rizz",
+        id: "CG30133"
+    },
+    {
+        title : "Sonic",
+        id: "CG30134"
+    },
+    {
+        title : "Coda",
+        id: "CG30135"
+    },
+    {
+        title : "Valve",
+        id: "CG30136"
+    },
+    {
+        title : "Flux",
+        id: "CG30137"
+    },
+    {
+        title : "Numa",
+        id: "CG30138"
+    }
+]
